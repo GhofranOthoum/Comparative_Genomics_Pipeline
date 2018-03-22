b8774c29313b5054c322f599b68b208558c11272
GhofranOthoum <36938512+GhofranOthoum@users.noreply.github.com>
2018-03-22 15:44:23 +0300

    Update phylo_tree.sh

--- phylo_tree.sh
+++ phylo_tree.sh
@@ -1,15 +1,32 @@
+#!/bin/sh
+#-------
+#check or download the following tools
+#get_homologues.pl from https://github.com/eead-csic-compbio/get_homologues/releases
+#muscle from https://www.drive5.com/muscle/downloads.htm
+#FASconCAT_v1.0.pl from https://github.com/PatrickKueck/FASconCAT
+#-------
 
-#get single-copy orthoulogous genes
- get_homologues.pl -e -M -d input_protein_files
+#get single-copy orthologous genes
+#get_homologues.pl -e -M -d $input_protein_folder
 #get alighnment of single-copy genes using muscle
- for file in /home/othoumgk/para/backup/*.faa
+#prottest-3.4.2.jar from 
+#raxmlHPC from https://sco.h-its.org/exelixis/web/software/raxml/index.html
+
+for file in $orthology_output_folder/*.faa
+
 do
-filename=$(basename $file .faa);
-muscle -in $file -out /home/othoumgk/para/muscleouput/$filename'.fas';
+	filename=$(basename $file .faa);
+	muscle -in $file -out ./tmp/output_dir/$filename'.fas';
 done
+
+cd ./tmp/output_dir/$filename
+
 # Concatinate all alighnments
 perl FASconCAT_v1.0.pl -s -p -p
+
 #find best amino acid replacement using prottest v3
-java -jar prottest-3.4.2.jar -i /projects/dragon/bacillus/B_lich/get_homologous/phylogeny/input_homologues/muscle_output/FcC_smatrix.phy  -o test_output
+java -jar prottest-3.4.2.jar -i ./tmp/output_dir/$filename/FcC_smatrix.phy  -o  prottest_output
+
 #run raxml
-raxmlHPC  -m PROTGAMMAWAG -J MR -p 12345 -x 12345 -N 100 -s FcC_smatrix.fas -n paratree -w raxml
+raxmlHPC  -m $Predicted_Model -J MR -p 12345 -x 12345 -N 100 -s ./tmp/output_dir/$filename/FcC_smatrix.phy -n paratree -w raxml
+

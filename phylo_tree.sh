1af48f90a268417fe5541705fed32fee20927f1d
GhofranOthoum <36938512+GhofranOthoum@users.noreply.github.com>
2018-03-29 13:51:53 +0300

    Update phylo_tree.sh

--- phylo_tree.sh
+++ phylo_tree.sh
@@ -1,33 +1,34 @@
-_#!/bin/sh
+#!/bin/sh
 #-------
-#check or download the following tools
+#check that the following tools are downloaded and loaded
 #get_homologues.pl from https://github.com/eead-csic-compbio/get_homologues/releases
 #muscle from https://www.drive5.com/muscle/downloads.htm
 #FASconCAT_v1.0.pl from https://github.com/PatrickKueck/FASconCAT
-#-------
+#prottest-3.4.2.jar from https://github.com/ddarriba/prottest3/blob/master/README.md
+#raxmlHPC from https://sco.h-its.org/exelixis/web/software/raxml/index.html
+#------
+
 
 #get single-copy orthologous genes
-#get_homologues.pl -e -M -d $input_protein_folder
-#get alighnment of single-copy genes using muscle
-#prottest-3.4.2.jar from 
-#raxmlHPC from https://sco.h-its.org/exelixis/web/software/raxml/index.html
+get_homologues.pl -e -M -d $input_protein_folder
 
+#get alighnment of single-copy genes using muscle 
 for file in $orthology_output_folder/*.faa
 
 do
 	filename=$(basename $file .faa);
 	muscle -in $file -out ./tmp/output_dir/$filename'.fas';
 done
+# Concatinate all alighnments
 
 cd ./tmp/output_dir/$filename
 
-# Concatinate all alighnments
 perl FASconCAT_v1.0.pl -s -p -p
 
 #find best amino acid replacement using prottest v3
 java -jar prottest-3.4.2.jar -i ./tmp/output_dir/$filename/FcC_smatrix.phy  -o  prottest_output
 
-#run raxml to get the best tree 
+#run raxml to get the best tree using 20 independent ML trees searches   
 raxmlHPC -m $Predicted_Model_from_prottest -p 12345 -# 20  -s ./tmp/output_dir/$filename/FcC_smatrix.phy  -n T1 -o Clostridioides_difficile_CD196
 #run raxml  with 100 bootsrap values
 

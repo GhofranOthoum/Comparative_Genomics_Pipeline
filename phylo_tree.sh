2ff63c0222be8048a58d422c6ed77e0281b15aee
GhofranOthoum <36938512+GhofranOthoum@users.noreply.github.com>
2018-03-29 13:32:15 +0300

    Update phylo_tree.sh

--- phylo_tree.sh
+++ phylo_tree.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+_#!/bin/sh
 #-------
 #check or download the following tools
 #get_homologues.pl from https://github.com/eead-csic-compbio/get_homologues/releases
@@ -27,6 +27,13 @@ perl FASconCAT_v1.0.pl -s -p -p
 #find best amino acid replacement using prottest v3
 java -jar prottest-3.4.2.jar -i ./tmp/output_dir/$filename/FcC_smatrix.phy  -o  prottest_output
 
-#run raxml
-raxmlHPC  -m $Predicted_Model -J MR -p 12345 -x 12345 -N 100 -s ./tmp/output_dir/$filename/FcC_smatrix.phy -n paratree -w raxml
+#run raxml to get the best tree 
+raxmlHPC -m $Predicted_Model_from_prottest -p 12345 -# 20  -s ./tmp/output_dir/$filename/FcC_smatrix.phy  -n T1 -o Clostridioides_difficile_CD196
+#run raxml  with 100 bootsrap values
+
+raxmlHPC -m $Predicted_Model_from_prottest -p 12345 -b 12345 -N 100 -s ./tmp/output_dir/$filename/FcC_smatrix.phy -n T2 -o Clostridioides_difficile_CD196
+
+#run raxml to generate the best tree with bootstrap support 
+
+ raxmlHPC -m $Predicted_Model_from_prottest -p 12345 -f b -t RAxML_bestTree.T1 -z RAxML_bootstrap.T2 -n T3 -o Clostridioides_difficile_CD196
 
